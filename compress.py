dab281a7be61ad8c7f9a306595290891e3e7f294
Antoni Jurga <antekjurga@gmail.com>
2024-09-26 14:58:08 +0200

    Cut out of full audio

--- compress.py
+++ compress.py
@@ -13,7 +13,7 @@ def stereo_to_mono(audio_data: np.ndarray) -> np.ndarray:
     
     return np.mean(audio_data, axis=1).astype(np.int16)
 
-def reduce_sample_rate(audio_data: np.ndarray, sample_rate: int, new_sample_rate: int) -> tuple[np.ndarray, int]:
+def reduce_sample_rate(audio_data: np.ndarray, sample_rate: int, new_sample_rate: int) -> np.ndarray:
     """
     Reduce the sample rate of the audio data to the given sample rate.
     """
@@ -24,7 +24,7 @@ def reduce_sample_rate(audio_data: np.ndarray, sample_rate: int, new_sample_rate
     new_length = int(len(audio_data) * new_sample_rate / sample_rate)
     compressed = scipy.signal.resample(audio_data, new_length).astype(np.int16)
     
-    return compressed, new_sample_rate
+    return compressed
 
 def save_audio_data(filename: str, sample_rate: int, audio_data: np.ndarray):
     """
@@ -48,15 +48,24 @@ def NCC(x: np.ndarray, y: np.ndarray) -> int:
     
     return cross_correlation
 
-# Load the data
-sample_rate_full, audio_data_full = wavfile.read('full-clip.wav')
-sample_rate_partial, audio_data_partial = wavfile.read('part.wav')
-
-compressed_full, compressed_sample_rate_full = reduce_sample_rate(stereo_to_mono(audio_data_full), sample_rate_full, 4400)
-compressed_partial, compressed_sample_rate_partial = reduce_sample_rate(stereo_to_mono(audio_data_partial), sample_rate_partial, 4400)
-
 STARING_PRECISION = 50
 PRECISION_REDUCTION = 5
+SAMPLE_RATE = 4410
+SEGMENT_LENGTH_SECONDS = 1
+
+# Load the data
+sample_rate_full, audio_data_full = wavfile.read('full-clip.wav')
+# sample_rate_partial, audio_data_partial = wavfile.read('part.wav')
+
+compressed_full = reduce_sample_rate(stereo_to_mono(audio_data_full), sample_rate_full, SAMPLE_RATE)
+
+# Get a random segment of the full audio data
+partial_length = SEGMENT_LENGTH_SECONDS * SAMPLE_RATE
+starting_time = np.random.randint(0, len(compressed_full) - partial_length)
+compressed_partial = compressed_full[starting_time:starting_time + partial_length]
+
+# Remove the partial audio data from the full audio data
+compressed_full = np.concatenate((compressed_full[:starting_time], compressed_full[starting_time + partial_length:]))
 
 precision = STARING_PRECISION
 similarities = []
