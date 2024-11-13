eba082247703781fcfdd8b0098b5ab439cc39948
ddf172 <jakubddfadamski@gmail.com>
2024-11-13 15:47:55 +0100

    refactored read function in AudioFileHandler.py

--- src/fileHandlers/AudioFileHandler.py
+++ src/fileHandlers/AudioFileHandler.py
@@ -1,10 +1,9 @@
 import os
 from src.dataTypes.Audio import Audio
-from src.dataTypes.AudioWAV import AudioWAV
-from src.fileHandlers.AudioFileConverter import AudioFileConverter
 from src.fileHandlers.FileHandlerAbstract import FileHandlerAbstract
+from src.fileHandlers.AudioWAVFileHandler import AudioWAVFileHandler
 from scipy.io import wavfile
-import numpy as np
+import re
 
 
 class AudioFileHandler(FileHandlerAbstract):
@@ -14,39 +13,22 @@ class AudioFileHandler(FileHandlerAbstract):
         super().__init__()
         pass
 
-    def read_as_wav(self, file_path: str) -> AudioWAV:
-        """ Read an audio file as a .wav file with normalised audio data in range (-1, 1) of type float32 """
-        
-        audioFileConverter = AudioFileConverter()
-        temp_path = audioFileConverter.convert_to_wav(file_path)
-        
-        sample_rate, audio = wavfile.read(temp_path)
-        dataNumberType = audio.dtype
-        
-        match dataNumberType:
-            case 'float32':
-                audio = audio
-            case 'int32':
-                audio = audio / 2**31
-            case 'int16':
-                audio = audio / 2**15
-            case 'uint8':
-                audio = (audio - 128) / 128
+    def read(self, file_path: str) -> Audio:
+
+        if not os.path.exists(file_path):
+            raise FileNotFoundError("File not found")
+
+        match self._get_extension(file_path):
+            case "wav":
+                return AudioWAVFileHandler().read(file_path)
+            case "mp3":
+                raise ValueError("mp3 format not supported")
             case _:
-                raise ValueError("Data type not supported")
-            
-        audio = audio.astype(np.float32)
-        
-        n_samples, n_channels = audio.shape
-        os.remove(temp_path)
-        
-        audioWAV = AudioWAV(audio=audio, format='wav', sample_rate=sample_rate, n_samples=n_samples, n_channels=n_channels)
-        
-        return audioWAV
-        
+                raise ValueError("File format not supported")
+
     def write(self, audio: Audio, file_path: str):
         """ Write an Audio object to a file, file_path must include the file extension """
-        match file_path.split(".")[-1]:
+        match self._get_extension(file_path):
             case "wav":
                 wavfile.write(file_path, audio.sample_rate, audio.audio)
             case _:
