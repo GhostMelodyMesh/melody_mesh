023e91b118e3188ff2e1bcf89ab502693b067e0d
ddf172 <jakubddfadamski@gmail.com>
2024-11-13 16:04:27 +0100

    deleted super.init() from init in AudioFileHandler.py

--- src/fileHandlers/AudioFileHandler.py
+++ src/fileHandlers/AudioFileHandler.py
@@ -9,7 +9,6 @@ class AudioFileHandler(FileHandlerAbstract):
     """ Handling of local audio files, reading different file formats, converting them into normalised format, saving them """
 
     def __init__(self):
-        super().__init__()
         pass
 
     def read(self, file_path: str) -> Audio:
