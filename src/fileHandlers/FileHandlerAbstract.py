5a0ebe573d4ef8757149936e02fa6566aef59c6d
ddf172 <jakubddfadamski@gmail.com>
2024-11-13 15:37:29 +0100

    Refactor of FileHandlerAbstract.py

--- src/fileHandlers/FileHandlerAbstract.py
+++ src/fileHandlers/FileHandlerAbstract.py
@@ -1,15 +1,18 @@
 from abc import ABC, abstractmethod
+from typing import TypeVar, Generic
+
+T = TypeVar('T')
 
 
-class FileHandlerAbstract(ABC):
+class FileHandlerAbstract(ABC, Generic[T]):
+    @abstractmethod
     def __init__(self):
         pass
 
-    # @abstractmethod
-    # def read(self):
-    #     pass
-
     @abstractmethod
-    def write(self):
+    def read(self, file_path: str) -> T:
         pass
 
+    @abstractmethod
+    def write(self, obj: T, file_path: str):
+        pass
