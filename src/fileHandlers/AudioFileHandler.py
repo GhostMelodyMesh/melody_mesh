83d5bd314261b123be3bf804dab4e3806287d1e0
ddf172 <jakubddfadamski@gmail.com>
2024-11-13 15:52:58 +0100

    refactored write function in AudioFileHandler.py

--- src/fileHandlers/AudioFileHandler.py
+++ src/fileHandlers/AudioFileHandler.py
@@ -1,13 +1,12 @@
 import os
 from src.dataTypes.Audio import Audio
+from src.dataTypes.AudioWAV import AudioWAV
 from src.fileHandlers.FileHandlerAbstract import FileHandlerAbstract
 from src.fileHandlers.AudioWAVFileHandler import AudioWAVFileHandler
-from scipy.io import wavfile
-import re
 
 
 class AudioFileHandler(FileHandlerAbstract):
-    """" Handling of local audio files, reading different file formats, converting them into normalised format, saving them """
+    """ Handling of local audio files, reading different file formats, converting them into normalised format, saving them """
 
     def __init__(self):
         super().__init__()
@@ -30,6 +29,11 @@ class AudioFileHandler(FileHandlerAbstract):
         """ Write an Audio object to a file, file_path must include the file extension """
         match self._get_extension(file_path):
             case "wav":
-                wavfile.write(file_path, audio.sample_rate, audio.audio)
+                if isinstance(audio, AudioWAV):
+                    AudioWAVFileHandler().write(audio, file_path)
+                else:
+                    raise ValueError("Wrong audio extension pair")
+            case "mp3":
+                raise ValueError("mp3 format not supported")
             case _:
                 raise ValueError("File format not supported")
