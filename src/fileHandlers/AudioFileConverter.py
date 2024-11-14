7b7e4ec8ac08604677b8af46440fe6afa33a89fd
ddf172 <jakubddfadamski@gmail.com>
2024-11-14 01:54:15 +0100

    Changed AudioFileConverter.py to not make redundant temp files

--- src/fileHandlers/AudioFileConverter.py
+++ src/fileHandlers/AudioFileConverter.py
@@ -1,6 +1,9 @@
 import re
 import shutil
 from pydub import AudioSegment
+from src.dataTypes.AudioType import AudioType
+from typing import Union
+
 
 class AudioFileConverter:
     """ Class to convert audio files to different formats """
@@ -8,7 +11,8 @@ class AudioFileConverter:
     def __init__(self):
         pass
 
-    def convert_to_mp3(self, file_path: str) -> str:
+    @staticmethod
+    def convert_to_mp3(file_path: str) -> str:
         """ Convert an audio file to mp3 format, WARNING: after reading the returned path, the temporary file should be deleted """
         extension = re.search(r'\.(\w+)$', file_path).group(1)
         match extension:
@@ -19,24 +23,17 @@ class AudioFileConverter:
                 audio.export(temp_file_path, format='mp3')
                 return temp_file_path
             case 'mp3':
-                # Copy the file to a temporary file
-                file_name = file_path.split('/')[-1]
-                temp_file_path = file_path.replace(file_name, 'temp.mp3')
-                shutil.copyfile(file_path, temp_file_path)
-                return temp_file_path
+                return file_path
             case _:
                 raise Exception("Unsupported audio file format")
 
-    def convert_to_wav(self, file_path: str) -> str:
+    @staticmethod
+    def convert_to_wav(file_path: str) -> str:
         """ Convert an audio file to wav format, WARNING: after reading the returned path, the temporary file should be deleted """
         extension = re.search(r'\.(\w+)$', file_path).group(1)
         match extension:
             case 'wav':
-                # Copy the file to a temporary file
-                file_name = file_path.split('/')[-1]
-                temp_file_path = file_path.replace(file_name, 'temp.wav')
-                shutil.copyfile(file_path, temp_file_path)
-                return temp_file_path
+                return file_path
             case 'mp3':
                 audio = AudioSegment.from_mp3(file_path)
                 file_name = file_path.split('/')[-1]
