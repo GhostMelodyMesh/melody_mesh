9573cdbf11723f52c2d63053b5a8db8a074a8fd9
ddf172 <jakubddfadamski@gmail.com>
2024-11-14 01:55:56 +0100

    Added delete_temp_file function that deletes file only if it ends with file.* Changed read in AudioFileHandler.py to match return_audio_type and use converter

--- src/fileHandlers/AudioFileHandler.py
+++ src/fileHandlers/AudioFileHandler.py
@@ -1,8 +1,12 @@
 import os
+import re
 from src.dataTypes.Audio import Audio
 from src.dataTypes.AudioWAV import AudioWAV
 from src.fileHandlers.AudioWAVFileProcessor import AudioWAVFileProcessor
 from src.utilities.get_extension import get_extension
+from src.dataTypes.AudioType import AudioType
+from src.fileHandlers.AudioFileConverter import AudioFileConverter
+from typing import Union
 
 
 class AudioFileHandler:
@@ -12,15 +16,23 @@ class AudioFileHandler:
         pass
 
     @staticmethod
-    def read(file_path: str) -> Audio:
+    def delete_temp_file(file_path: str):
+        if re.search(r'temp\.\w+$', file_path):
+            os.remove(file_path)
+
+    @staticmethod
+    def read(file_path: str, return_audio_type: Union[AudioType, None] = None) -> Audio:
         """ Read an audio file from a file path, file_path must include the file extension """
         if not os.path.exists(file_path):
             raise FileNotFoundError("File not found")
 
-        match get_extension(file_path):
-            case "wav":
-                return AudioWAVFileProcessor().read(file_path)
-            case "mp3":
+        match return_audio_type:
+            case AudioType.WAV:
+                file_path = AudioFileConverter().convert_to_wav(file_path)
+                audio = AudioWAVFileProcessor().read(file_path)
+                AudioFileHandler.delete_temp_file(file_path)
+                return audio
+            case AudioType.MP3:
                 raise ValueError("mp3 format not supported")
             case _:
                 raise ValueError("File format not supported")
