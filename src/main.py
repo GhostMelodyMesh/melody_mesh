9f5602941151bfd2a389295e1ff4552d9cda29d5
rybijakKarpiowy <104085107+rybijakKarpiowy@users.noreply.github.com>
2024-11-18 16:30:14 +0100

    Update main.py

--- src/main.py
+++ src/main.py
@@ -1,9 +1,13 @@
-from src.compressors.CompressorWAVBaseline import CompressorWAVBaseline
+from src.dataTypes.AudioFormat import AudioFormat
 from src.fileHandlers.AudioFileHandler import AudioFileHandler
 
-
 print("Hello World!")
-c = CompressorWAVBaseline()
-c.compress("rawAudio/file0.mp3")
-fReader = AudioFileHandler()
-audio = fReader.read_as_wav("rawAudio/file0.mp3")
+# c = CompressorWAVBaseline()
+# c.compress("rawAudio/file0.mp3")
+# fReader = AudioFileHandler()
+# audio = fReader.read_as_wav("rawAudio/file0.mp3")
+
+fp = AudioFileHandler()
+audio1 = fp.read(file_path="compressedAudio/file0.wav", return_audio_type=AudioFormat.WAV)
+# save
+fp.write(audio1, file_path="compressedAudio/file0.wav")
