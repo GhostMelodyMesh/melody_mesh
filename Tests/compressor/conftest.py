b876e9d6ba4fc0355e9d3e2e26378887c974997b
rybijakKarpiowy <104085107+rybijakKarpiowy@users.noreply.github.com>
2024-11-19 22:34:55 +0100

    Updated gitignore, removed obsolete  test

--- Tests/compressor/conftest.py
+++ Tests/compressor/conftest.py
@@ -1,7 +1,8 @@
 import pytest
-from src.compressors.CompressorWAVBaseline_old import CompressorWAVBaseline
+# from src.compressors.CompressorWAVBaseline_old import CompressorWAVBaseline
 
 
 @pytest.fixture(scope='session')
 def compressor():
-    return CompressorWAVBaseline()
+    # return CompressorWAVBaseline()
+    pass
