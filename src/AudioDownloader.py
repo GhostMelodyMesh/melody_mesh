e0226fe7e5bea1c894453d78eae9db512b200c3c
Klus3kk <lukasz.bielaszewski@onet.pl>
2024-11-27 00:48:44 +0100

    Fix Pool implementation, improved get_links method.

--- src/AudioDownloader.py
+++ src/AudioDownloader.py
@@ -1,30 +1,39 @@
+"""
+We've got some problems:
+1. Yt_dlp library probably doesn't work - it needs to be checked out
+2. If we want to download songs from Spotify we need to have Spotify API for that,
+and there's probably no way arround it.
+
+Some insights:
+1. I've fixed Pool, now it should work. I've just implemented Pool locally in the func.
+2. Also, we should write types in functions, it's a better practise imo.
+3. I fixed get_links, cuz we need to separate name of the song from the link.
+"""
+
 import os
 import subprocess 
 import random
 from abc import ABC, abstractmethod
-from spotdl import Spotdl # It finds songs from the Spotify playlists on Youtube and downloads them
+from spotdl import Spotdl # UPDATE: We need client_id and client_secret from Spotify Dev for it to work....and I don't think there's another way
 import yt_dlp # For now the best solution, it works for Youtube and possibly SoundCloud
 from yt_dlp.utils import DownloadError
 from multiprocessing import Pool # For downloading files concurrently
 
-
 class DataGenerator(ABC):
     """ Get file links from '.txt' and download them to rawAudio folder """
-    
     @abstractmethod
     def get_links(self, file_path: str) -> list: # It is better to write types of data and return types in the functions
         """Fetch file links from the source"""
         pass 
     
-
 class TxtLinkLoader(DataGenerator):
-    """Fetch file links from a .txt file."""
+    """Fetch file links from a .txt file. 'name; URL' format"""
     
     def get_links(self, file_path: str) -> list:
-        """Retrieve links line by line from the text file"""
+        """Retrieve links line by line from the text file, with ignoring track names"""
         try:
             with open(file_path, "r") as f:
-                return [line.strip() for line in f if line.strip()]
+                return [line.split(';')[1].strip() for line in f if ';' in line]
         except FileNotFoundError:
             print(f"File not found: {file_path}")
             return []
@@ -36,13 +45,18 @@ class AudioDownloader:
     # AUDIO_FOLDER = "rawAudio"
     # LINKS_PATH = "./audioFilesLinks.txt"
     
-    def __init__(self, data_generator: DataGenerator, audio_folder: str="rawAudio", num_files: int = 10, links_path: str ="./audioFilesLinks.txt"):
+    def __init__(self, 
+                 data_generator: DataGenerator, 
+                 audio_folder: str="rawAudio", 
+                 num_files: int = 10, 
+                 links_path: str ="./audioFilesLinks.txt",
+                 client_id: str = None,
+                 client_secret: str = None):
         self.audio_folder = audio_folder   # It's better to initiate those here
         self.num_files = num_files
         self.links_path = links_path
         self.data_generator = data_generator
-        self.spotdl = Spotdl() # For Spotify
-        self.pool = Pool()
+        # self.spotdl = Spotdl(client_id=client_id, client_secret=client_secret) # For Spotify
     
     def folder_exists(self):
         """Ensure that the audio_folder exists."""
@@ -108,10 +122,10 @@ class AudioDownloader:
             urls_scraped = []
             for i in range(num_files):
                 urls_scraped.append(urls[random_indices[i]])
-            self.pool.map(self.download_audio, urls_scraped)     
+            with Pool() as pool:
+                pool.map(self.download_audio, urls_scraped)  
         else:
-            self.pool.map(self.download_audio, urls)
-            
-        
-        
-        
+            with Pool() as pool:
+                pool.map(self.download_audio, urls)
+
+
